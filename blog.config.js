46287a225d05ea64c958ce333c60f895b8dda6aa
Jiaxin Peng <peng9498@gmail.com>
2023-07-03 12:34:13 +0100

    update the footer

--- blog.config.js
+++ blog.config.js
@@ -19,6 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Jiaxin Peng', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'A father', // 作者简介
+  WEBURL: process.env.NEXT_PUBLIC_WEBURL || 'https://jiaxinpeng.com', // 您的网站地址
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://pengjiaxin.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Jiaxin Peng, Blog', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,5 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+// document.getElementById('draggableBox').style.left = '0px'
+//
+//
+// document.getElementById('draggableBox').style.top = ''
