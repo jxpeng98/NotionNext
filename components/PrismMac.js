ecea4902014ca7e725dfefa64a0b7ce85da3d839
Jiaxin Peng <peng9498@gmail.com>
2023-07-18 11:23:28 +0100

    fix indent bug

--- components/PrismMac.js
+++ components/PrismMac.js
@@ -55,7 +55,7 @@ const PrismMac = () => {
         }
         renderPrismMac()
         renderMermaid()
-      renderCollapseCode()
+        renderCollapseCode()
       })
     }
     handleDarkModeChange()
