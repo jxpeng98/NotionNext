164420bd04192dfdb30592cde77665bea61cc434
Jiaxin Peng <peng9498@gmail.com>
2023-07-30 11:17:09 +0100

    update the purgecss again

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,54 @@
-// const purgeJs = require('purgecss-from-js')
-// const purgeHTML = require('purgecss-from-html')
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
-
+const purgeJs = require('purgecss-from-js')
+const purgeHTML = require('purgecss-from-html')
 // module.exports = {
-//   plugins: [
-//     // 'postcss-flexbugs-fixes',
-//     // ['postcss-preset-env', {
-//     //   autoprefixer: {
-//     //     flexbox: 'no-2009'
-//     //   },
-//     //   stage: 3,
-//     //   features: {
-//     //     'custom-properties': false
-//     //   }
-//     // }],
-//     // 'tailwindcss',
-//     // 'autoprefixer'
-//     // ['@fullhuman/postcss-purgecss',
-//     //   process.env.NODE_ENV === 'production'
-//     //     ? {
-//     //         content: [
-//     //           './pages/**/*.{js,jsx,ts,tsx}',
-//     //           './components/**/*.{js,jsx,ts,tsx}',
-//     //           './lib/**/*.{js,jsx,ts,tsx}'
-//     //         ],
-//     //         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-//     //         safelist: ['html', 'body']
-//     //       }
-//     //     : false
-//     // ]
-//   ]
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {}
+//   }
 // }
+
+module.exports = {
+  plugins: [
+    'postcss-flexbugs-fixes',
+    ['postcss-preset-env', {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false
+      }
+    }],
+    'tailwindcss',
+    'autoprefixer',
+    ['@fullhuman/postcss-purgecss',
+      process.env.NODE_ENV === 'production'
+        ? {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+              './lib/**/*.{js,jsx,ts,tsx}'
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+            extractors: [
+              {
+                extractor: purgeJs,
+                extensions: ['js']
+              },
+              {
+                extractor: purgeHTML,
+                extensions: ['html']
+              }
+            ],
+            safelist: {
+              standard: ['random', 'yep', 'button', /^nav-/],
+              deep: [],
+              greedy: [],
+              keyframes: [],
+              variables: []
+            }
+          }
+        : false
+    ]
+  ]
+}
