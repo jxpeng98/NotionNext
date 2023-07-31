2c30ee59523dec15d92f59780b0928531e7b406e
Jiaxin Peng <peng9498@gmail.com>
2023-07-31 08:07:19 +0100

    add purgecss

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ import '@/styles/notion.css' //  重写部分样式
 import { GlobalContextProvider } from '@/lib/global'
 
 import AOS from 'aos'
+
 import 'aos/dist/aos.css' // You can also use <link> for styles
 import dynamic from 'next/dynamic'
 
@@ -22,7 +23,13 @@ const ExternalPlugins = dynamic(() => import('@/components/ExternalPlugins'))
 
 const MyApp = ({ Component, pageProps }) => {
   useEffect(() => {
-    AOS.init()
+    AOS.init({
+      easing: "ease-out-cubic",
+      delay: 200,
+      duration: 300,
+      once: true,
+      offset: 50,
+    })
   }, [])
 
   return (
--- postcss.config.js
+++ postcss.config.js
@@ -1,73 +1,73 @@
 
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
-// const purgeJs = require('purgecss-from-js')
-// const purgeHTML = require('purgecss-from-html')
-
 // module.exports = {
-//   plugins: [
-//     'postcss-flexbugs-fixes',
-//     ['postcss-preset-env', {
-//       autoprefixer: {
-//         flexbox: 'no-2009'
-//       },
-//       stage: 3,
-//       features: {
-//         'custom-properties': false
-//       }
-//     }],
-//     'tailwindcss',
-//     'autoprefixer',
-//     // ['cssnano',
-//     //   {
-//     //     preset: 'default',
-//     //     discardComments: {
-//     //       removeAll: true
-//     //     }
-//     //   }
-//     // ],
-//     ['@fullhuman/postcss-purgecss',
-//       process.env.NODE_ENV === 'production'
-//         ? {
-//             content: [
-//               './pages/**/*.{js,jsx,ts,tsx}',
-//               './components/**/*.{js,jsx,ts,tsx}',
-//               './lib/**/*.{js,jsx,ts,tsx}',
-//               './themes/**/*.{js,jsx,ts,tsx}',
-//               './public/**/*.{js,jsx,ts,tsx,html}'
-//             ],
-//             css: [
-//               './styles/**/*.{css,scss,sass,less,styl}',
-//               './public/**/*.{css,scss,sass,less,styl}'
-//             ],
-//             extractors: [
-//               {
-//                 extractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-//                 extensions: ['html', 'js'] // file extensions this extractor should be applied to
-//               },
-//               {
-//                 extractor: purgeJs,
-//                 extensions: ['js', 'jsx', 'ts', 'tsx']
-//               },
-//               {
-//                 extractor: purgeHTML,
-//                 extensions: ['html']
-//               }
-//             ],
-//             safelist: {
-//               standard: ['body', 'html', 'article'],
-//               deep: [/white$/, /black$/, /gray$/],
-//               greedy: [/yellow$/, /indigo$/, /post$/, /(data-aos)/],
-//               keyframes: true,
-//               variables: true,
-//               rejected: true
-//             }
-//           }
-//         : false
-//     ]
-//   ]
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {}
+//   }
 // }
+const purgeJs = require('purgecss-from-js')
+const purgeHTML = require('purgecss-from-html')
+
+module.exports = {
+  plugins: [
+    'postcss-flexbugs-fixes',
+    ['postcss-preset-env', {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false
+      }
+    }],
+    'tailwindcss',
+    'autoprefixer',
+    // ['cssnano',
+    //   {
+    //     preset: 'default',
+    //     discardComments: {
+    //       removeAll: true
+    //     }
+    //   }
+    // ],
+    ['@fullhuman/postcss-purgecss',
+      process.env.NODE_ENV === 'production'
+        ? {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+              './lib/**/*.{js,jsx,ts,tsx}',
+              './themes/**/*.{js,jsx,ts,tsx}',
+              './public/**/*.{js,jsx,ts,tsx,html}'
+            ],
+            css: [
+              './styles/**/*.{css,scss,sass,less,styl}',
+              './public/**/*.{css,scss,sass,less,styl}'
+            ],
+            extractors: [
+              {
+                extractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+                extensions: ['html', 'js'] // file extensions this extractor should be applied to
+              },
+              {
+                extractor: purgeJs,
+                extensions: ['js', 'jsx', 'ts', 'tsx']
+              },
+              {
+                extractor: purgeHTML,
+                extensions: ['html']
+              }
+            ],
+            safelist: {
+              standard: ['body', 'html', 'article', 'main'],
+              deep: [/white$/, /black$/, /gray$/, /data-aos/, /aos-init$/, /aos-animate$/, /fade-up$/, /fade-down$/, /fade-right$/, /fade-left$/, /aos-duration-/],
+              greedy: [/yellow$/, /indigo$/, /post$/],
+              keyframes: true,
+              variables: true,
+              rejected: true
+            }
+          }
+        : false
+    ]
+  ]
+}
